4864900d4d61f8c5b0df6241b01308d3563c8af0
Herbert Höller <marco_edelmayer@gmx.at>
2023-02-07 15:49:04 +0100

    Es wurde die Summe eingefügt

---
+++ src/main/java/at/wifi/swdev/gitproject/Taschenrechner.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package at.wifi.swdev.gitproject;
+
+/**
+ *
+ * @author dev
+ */
+public class Taschenrechner {
+
+    
+public static void main(String[] args) {
+		double ergebnis;
+		double zahl1 = 4;
+                double zahl2 =3;
+
+		ergebnis=zahl1+zahl2;
+		System.out.println("Das Ergebnis ist: " + ergebnis);
+	}
+}
